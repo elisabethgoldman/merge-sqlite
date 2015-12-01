8d23aaf407ee8e68be5687f47255f14e023adc1c
Jeremiah H. Savage <jeremiahsavage@gmail.com>
2015-12-01 14:25:19 -0600

    input/output -> source/destination

--- main.py
+++ main.py
@@ -9,48 +9,48 @@ import sqlalchemy
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-def dump_data(output_session, output_engine, input_session, input_engine):
-    input_table_list = input_engine.table_names()
-    input_engine._metadata.reflect(input_engine) # get columns from existing table
-    for input_table_name in input_table_list:
-        input_table = sqlalchemy.Table(input_table_name, input_engine._metadata)
-        output_table = sqlalchemy.Table(input_table_name, output_engine._metadata)
-        for column in input_table.columns:
-            output_table.append_column(column.copy())
-        Base = declarative_base()
-        tables = Base.metadata.tables;
+def dump_data(destination_session, destination_engine, source_session, source_engine):
+    source_table_list = source_engine.table_names()
+    source_engine._metadata.reflect(source_engine) # get columns from existing table
+    for source_table_name in source_table_list:
+        source_table = sqlalchemy.Table(source_table_name, source_engine._metadata)
+        destination_table = sqlalchemy.Table(source_table_name, destination_engine._metadata)
+        #for column in source_table.columns:
+        #    destination_table.append_column(column.copy())
+        #Base = declarative_base()
+        #tables = Base.metadata.tables;
         IPython.embed()
-        data = input_engine.execute(tables[input_table_name].select()).fetchall()
-        print('data=\n%s' % data)
-        if data:
-            output_engine(tables[input_table_name].insert(), data)
-        output_table.create()
+        #data = source_engine.execute(tables[source_table_name].select()).fetchall()
+        #print('data=\n%s' % data)
+        #if data:
+        #    destination_engine(tables[source_table_name].insert(), data)
+        destination_table.create()
                 
         
 def main():
     parser = argparse.ArgumentParser('merge an arbitrary number of sqlite files')
-    parser.add_argument('-i', '--input_sqlite', action='append', required=True)
+    parser.add_argument('-s', '--source_sqlite', action='append', required=True)
     parser.add_argument('-u', '--uuid', required=True)
     args = parser.parse_args()
     
-    input_sqlite_list = args.input_sqlite
+    source_sqlite_list = args.source_sqlite
     uuid = args.uuid
     
-    output_sqlite = uuid+'.db'
-    output_engine_path = 'sqlite:///' + output_sqlite
-    output_engine = sqlalchemy.create_engine(output_engine_path, isolation_level='SERIALIZABLE')
-    output_Session = sessionmaker(bind=output_engine)
-    output_session = output_Session()
-    output_engine._metadata = sqlalchemy.MetaData(bind=output_engine)
+    destination_sqlite = uuid+'.db'
+    destination_engine_path = 'sqlite:///' + destination_sqlite
+    destination_engine = sqlalchemy.create_engine(destination_engine_path, isolation_level='SERIALIZABLE')
+    destination_Session = sessionmaker(bind=destination_engine)
+    destination_session = destination_Session()
+    destination_engine._metadata = sqlalchemy.MetaData(bind=destination_engine)
     
-    for input_sqlite in input_sqlite_list:
-        print('input_sqlite=%s' % input_sqlite)
-        input_engine_path = 'sqlite:///' + input_sqlite
-        input_engine = sqlalchemy.create_engine(input_engine_path, isolation_level='SERIALIZABLE')
-        input_Session = sessionmaker(bind=input_engine)
-        input_session = input_Session()
-        input_engine._metadata = sqlalchemy.MetaData(bind=input_engine)
-        dump_data(output_session, output_engine, input_session, input_engine)
+    for source_sqlite in source_sqlite_list:
+        print('source_sqlite=%s' % source_sqlite)
+        source_engine_path = 'sqlite:///' + source_sqlite
+        source_engine = sqlalchemy.create_engine(source_engine_path, isolation_level='SERIALIZABLE')
+        source_Session = sessionmaker(bind=source_engine)
+        source_session = source_Session()
+        source_engine._metadata = sqlalchemy.MetaData(bind=source_engine)
+        dump_data(destination_session, destination_engine, source_session, source_engine)
         
         
 
