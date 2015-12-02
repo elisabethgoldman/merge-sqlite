cd8ae2e817dca18091cba9cc6e558242ae85dd7e
Jeremiah H. Savage <jeremiahsavage@gmail.com>
2015-12-02 16:58:52 -0600

    works

--- main.py
+++ main.py
@@ -1,32 +1,11 @@
 #!/usr/bin/env python3
 
 import argparse
-import os
-import pdb
+#import os
+import sqlite3
 import IPython
 
-import sqlalchemy
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
 
-def dump_data(destination_session, destination_engine, source_session, source_engine):
-    source_table_list = source_engine.table_names()
-    source_engine._metadata.reflect(source_engine) # get columns from existing table
-    for source_table_name in source_table_list:
-        source_table = sqlalchemy.Table(source_table_name, source_engine._metadata)
-        destination_table = sqlalchemy.Table(source_table_name, destination_engine._metadata)
-        #for column in source_table.columns:
-        #    destination_table.append_column(column.copy())
-        #Base = declarative_base()
-        #tables = Base.metadata.tables;
-        IPython.embed()
-        #data = source_engine.execute(tables[source_table_name].select()).fetchall()
-        #print('data=\n%s' % data)
-        #if data:
-        #    destination_engine(tables[source_table_name].insert(), data)
-        destination_table.create()
-                
-        
 def main():
     parser = argparse.ArgumentParser('merge an arbitrary number of sqlite files')
     parser.add_argument('-s', '--source_sqlite', action='append', required=True)
@@ -36,24 +15,60 @@ def main():
     source_sqlite_list = args.source_sqlite
     uuid = args.uuid
     
-    destination_sqlite = uuid+'.db'
-    destination_engine_path = 'sqlite:///' + destination_sqlite
-    destination_engine = sqlalchemy.create_engine(destination_engine_path, isolation_level='SERIALIZABLE')
-    destination_Session = sessionmaker(bind=destination_engine)
-    destination_session = destination_Session()
-    destination_engine._metadata = sqlalchemy.MetaData(bind=destination_engine)
+    destination_sqlite_path = uuid+'.db'
+    destination_conn = sqlite3.connect(destination_sqlite_path)
+    destination_cur = destination_conn.cursor()
     
-    for source_sqlite in source_sqlite_list:
-        print('source_sqlite=%s' % source_sqlite)
-        source_engine_path = 'sqlite:///' + source_sqlite
-        source_engine = sqlalchemy.create_engine(source_engine_path, isolation_level='SERIALIZABLE')
-        source_Session = sessionmaker(bind=source_engine)
-        source_session = source_Session()
-        source_engine._metadata = sqlalchemy.MetaData(bind=source_engine)
-        dump_data(destination_session, destination_engine, source_session, source_engine)
-        
-        
+    for source_sqlite_path in source_sqlite_list:
+        source_conn = sqlite3.connect(source_sqlite_path)
+        source_cur = source_conn.cursor()
+        source_cur.execute('SELECT * from sqlite_master')
+        source_master = source_cur.fetchall()
+        print('source_master=%s' % source_master)
+        source_tables = list(filter(lambda r: r[0] == 'table', source_master))
+        print('source_tables=%s' % list(source_tables))
+        print('len(source_tables)=%s' % len(source_tables))
+        for source_table in source_tables:
+            #print('source_table=%s' % str(source_table))
+            #IPython.embed()
+            # create table structure
+            destination_cur.execute(source_table[4]) #4 is sql
 
-
+            # move data
+            #source_cols = source_table[4]
+            #print('source_cols=%s' % source_cols)
+            cursor = source_cur.execute('select * from %s' % source_table[1]) # 1 is name
+            names = tuple(map(lambda x: x[0], cursor.description))
+            #print('names=%s' % str(names))
+            for source_row in source_cur:
+                #print('source_row=%s' % str(source_row))
+                source_tuple = eval('(' + str(source_row).strip('(').strip('(').strip(')').strip(')') + ')')
+                source_str = str(source_row).strip('(').strip('(').strip(')').strip(')')
+                #print('source_tpl=%s' % str(source_tuple))
+                source_list = list(source_tuple)
+                #print('source_lst=%s' % str(source_list))
+                #print('len(source_list)==%s' % len(source_list))
+                new_str = str()
+                for i, source_field in enumerate(source_list):
+                    source_field_str = str(source_field).strip()
+                    #print('source_field, source_type=%s, %s' % (str(source_field), type(source_field)))
+                    if source_field == None:
+                        #print('None: %s' % source_field)
+                        new_str += '\'\','
+                    elif type(source_field) is str:
+                        new_str += '"'+str(source_field)+'"'+','
+                    else:
+                        new_str += str(source_field)+','
+                new_str = new_str.rstrip(',')
+                #print('new_str=%s' % new_str)
+                query = 'INSERT INTO %(tbl)s %(col)s VALUES (%(phold)s)' % {
+                    'tbl' : source_table[1], # 1 is name
+                    'col' : names,
+                    'phold' : new_str
+                    }
+                #print('query=%s' % query)
+                destination_cur.execute(query)
+                destination_conn.commit()
+                                        
 if __name__ == '__main__':
     main()
