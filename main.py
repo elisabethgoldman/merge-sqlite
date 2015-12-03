66465241a4a6b6f5afb4dfb0fd740371d8769934
Jeremiah H. Savage <jeremiahsavage@gmail.com>
2015-12-03 10:35:51 -0600

    faster implementation

--- main.py
+++ main.py
@@ -6,6 +6,25 @@ import sqlite3
 import IPython
 
 
+def get_row_str(source_tuple):
+    source_list = list(source_tuple)
+    #print('source_lst=%s' % str(source_list))
+    #print('len(source_list)==%s' % len(source_list))
+    new_str = str()
+    for i, source_field in enumerate(source_list):
+        source_field_str = str(source_field).strip()
+        #print('source_field, source_type=%s, %s' % (str(source_field), type(source_field)))
+        if source_field == None:
+            #print('None: %s' % source_field)
+            new_str += '\'\','
+        elif type(source_field) is str:
+            new_str += '"'+str(source_field)+'"'+','
+        else:
+            new_str += str(source_field)+','
+    new_str = new_str.rstrip(',')
+    return new_str
+
+
 def main():
     parser = argparse.ArgumentParser('merge an arbitrary number of sqlite files')
     parser.add_argument('-s', '--source_sqlite', action='append', required=True)
@@ -33,7 +52,6 @@ def main():
             #IPython.embed()
             # create table structure
             destination_cur.execute(source_table[4]) #4 is sql
-
             # move data
             #source_cols = source_table[4]
             #print('source_cols=%s' % source_cols)
@@ -41,33 +59,22 @@ def main():
             names = tuple(map(lambda x: x[0], cursor.description))
             #print('names=%s' % str(names))
             for source_row in source_cur:
-                #print('source_row=%s' % str(source_row))
+                phold = ','.join(('?',) * len(source_row))
+                #print('phold=%s' % phold)
+                #print('str(source_row)=%s' % str(source_row))
+                #print('str(source_row)=%s' % type(source_row))
                 source_tuple = eval('(' + str(source_row).strip('(').strip('(').strip(')').strip(')') + ')')
                 source_str = str(source_row).strip('(').strip('(').strip(')').strip(')')
                 #print('source_tpl=%s' % str(source_tuple))
-                source_list = list(source_tuple)
-                #print('source_lst=%s' % str(source_list))
-                #print('len(source_list)==%s' % len(source_list))
-                new_str = str()
-                for i, source_field in enumerate(source_list):
-                    source_field_str = str(source_field).strip()
-                    #print('source_field, source_type=%s, %s' % (str(source_field), type(source_field)))
-                    if source_field == None:
-                        #print('None: %s' % source_field)
-                        new_str += '\'\','
-                    elif type(source_field) is str:
-                        new_str += '"'+str(source_field)+'"'+','
-                    else:
-                        new_str += str(source_field)+','
-                new_str = new_str.rstrip(',')
                 #print('new_str=%s' % new_str)
+                #new_str = get_row_str(source_tuple)
                 query = 'INSERT INTO %(tbl)s %(col)s VALUES (%(phold)s)' % {
                     'tbl' : source_table[1], # 1 is name
                     'col' : names,
-                    'phold' : new_str
+                    'phold' : phold
                     }
                 #print('query=%s' % query)
-                destination_cur.execute(query)
+                destination_cur.execute(query, source_row)
                 destination_conn.commit()
                                         
 if __name__ == '__main__':
