4273913835d43847c96c3d5d20639ff5cb126b04
Jeremiah H. Savage <jeremiahsavage@gmail.com>
2016-01-05 17:16:04 -0600

    use cdis_pipe_utils

--- merge_sqlite/main.py
+++ merge_sqlite/main.py
@@ -1,17 +1,12 @@
 #!/usr/bin/env python3
 
 import argparse
+
 import os
-import subprocess
-import sys
 
-def do_shell_command(cmd, stdout=subprocess.STDOUT, stderr=subprocess.PIPE):
-    try:
-        output = subprocess.check_output(cmd, stderr=subprocess.STDOUT, shell=True)
-    except Exception as e:
-        sys.exit('failed cmd: %s' % str(cmd))
-    return output
-
+from cdis_pipe_utils import df_util
+from cdis_pipe_utils import pipe_util
+from cdis_pipe_utils import time_util
 
 def main():
     parser = argparse.ArgumentParser('merge an arbitrary number of sqlite files')
@@ -22,20 +17,32 @@ def main():
     source_sqlite_list = args.source_sqlite
     uuid = args.uuid
 
-    destination_sqlite_path = uuid+'.db'
-    print('destination_sqlite_path=%s' % destination_sqlite_path)
-    for source_sqlite_path in source_sqlite_list:
-        ('source_sqlite_path=%s' % source_sqlite_path)
-        source_sqlite_name = os.path.splitext(os.path.basename(source_sqlite_path))[0]
-        source_dump_path = source_sqlite_name + '.sql'
-        #dump
-        cmd = ['sqlite3', source_sqlite_path, "\'.dump\'", '>', source_dump_path ]
-        shell_cmd = ' '.join(cmd)
-        do_shell_command(shell_cmd)
-        #load
-        cmd = ['sqlite3', destination_sqlite_path, '<', source_dump_path]
-        shell_cmd = ' '.join(cmd)
-        do_shell_command(shell_cmd)
-                                        
+    tool_name = 'merge_sqlite'
+    logger = pipe_util.setup_logging(tool_name, args, uuid)
+    engine = pipe_util.setup_db(uuid+'_log')
+
+    step_dir = os.getcwd()
+    if pipe_util.already_step(step_dir, uuid + '_db', logger):
+        logger.info('already completed step `merge_sqlite`')
+    else:
+        logger.info('running step `merge_sqlite`')
+        for source_sqlite_path in source_sqlite_list:
+            ('source_sqlite_path=%s' % source_sqlite_path)
+            source_sqlite_name = os.path.splitext(os.path.basename(source_sqlite_path))[0]
+
+            #dump
+            source_dump_path = source_sqlite_name + '.sql'
+            cmd = ['sqlite3', source_sqlite_path, "\'.dump\'", '>', source_dump_path ]
+            shell_cmd = ' '.join(cmd)
+            pipe_util.do_shell_command(shell_cmd, logger)
+
+            #load
+            destination_sqlite_path = uuid + '.db'
+            cmd = ['sqlite3', destination_sqlite_path, '<', source_dump_path]
+            shell_cmd = ' '.join(cmd)
+            pipe_util.do_shell_command(shell_cmd, logger)
+        pipe_util.create_already_step(step_dir, uuid + '_db', logger)
+    return
+
 if __name__ == '__main__':
     main()
