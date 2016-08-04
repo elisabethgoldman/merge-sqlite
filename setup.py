c56a5ad6ef6f9bd76bf7ee22f44d73352ed18913
Jeremiah H. Savage <jeremiahsavage@gmail.com>
2016-08-04 11:27:09 -0500

    enable binary

--- setup.py
+++ setup.py
@@ -5,16 +5,11 @@ from setuptools import setup, find_packages
 setup(name = 'merge_sqlite',
       author = 'Jeremiah H. Savage',
       author_email = 'jeremiahsavage@gmail.com',
-      version = 0.22,
+      version = 0.23,
       description = 'merge arbitrary number of sqlite files',
       url = 'https://github.com/jeremiahsavage/merge_sqlite',
       license = 'Apache 2.0',
       packages = find_packages(),
-      install_requires = [
-          'pandas',
-          'sqlalchemy',
-          'cdis_pipe_utils'
-      ],
       classifiers = [
           'Development Status :: 3 - Alpha',
           'Intended Audience :: Developers',
@@ -23,4 +18,7 @@ setup(name = 'merge_sqlite',
           'Programming Language :: Python :: 2',
           'Programming Language :: Python :: 3',
       ],
+      entry_points={
+          'console_scripts': ['merge_sqlite=merge_sqlite.__main__:main']
+      },
 )
